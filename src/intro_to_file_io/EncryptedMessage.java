3e421505d258ea4ff7e0b95f28d39d6cc2e71605
pigchick3n <League@AdministorsiMac.attlocal.net>
2017-12-17 11:30:32 -0800

    Finished part 1 of the encrypted message! :)

--- src/intro_to_file_io/EncryptedMessage.java
+++ src/intro_to_file_io/EncryptedMessage.java
@@ -5,20 +5,44 @@ import java.io.IOException;
 import java.util.List;
 
 import javax.swing.JOptionPane;
+import javax.xml.ws.handler.MessageContext;
 
 public class EncryptedMessage {
 
-File file = new File("file.txt");
+File file = new File("encryption.txt");
+static String morseA = " *- ";
+static String morseE = " * ";
+static String morseI = " ** ";
+static String morseO = " --- ";
+static String morseU = " **- ";
 
 public static void main(String[] args) {
 	
 	String text = JOptionPane.showInputDialog(null, "What do you want to put in the file?");
+
+	char lastLetter = text.charAt(text.length() - text.length());
+	char firstLetter = text.charAt(0);
+	
+	//String change1 = text.replaceAll("a", "*");
+	//String change2 = change1.replaceAll("e", "$");
+	String change1 = text.replaceAll("a", morseA);
+	System.out.println(change1);
+	String change2 = change1.replaceAll("e", morseE);
+	System.out.println(change2);
+	String change3 = change2.replaceAll("i", morseI);
+	System.out.println(change3);
+	String change4 = change3.replaceAll("o", morseO);
+	System.out.println(change4);
+	String change5 = change4.replaceAll("u", morseU);
+	System.out.println(change5);
+	String newText = change5;
+	
 	
 	try {
 
-		FileWriter orange = new FileWriter("src/intro_to_file_io/file.txt");
+		FileWriter orange = new FileWriter("src/intro_to_file_io/encryption.txt");
 	
-		orange.write(text);
+		orange.write(newText);
 			
 		orange.close();
 	} catch (IOException e) {
@@ -34,10 +58,8 @@ for (int i = 0; i < text.length(); i++) {
 }
 
 
+
 	
-	System.out.println(text.length());
-	System.out.println(text.charAt(text.length() - 1));
-	System.out.println(text.charAt(0));
 	
 	
 	
